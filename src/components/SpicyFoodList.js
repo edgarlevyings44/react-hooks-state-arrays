1e0683d76999c544f2e839e7700d44e648dc0731
Levy <edgarlevy81@gmail.com>
2023-09-02 00:42:33 +0300

    Lesson Learnt

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
 import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
+// Funtions to add food
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
     console.log(newFood);
+  }
+// Functions to remove food
+  function handleLiClick(id) {
+    const newFoodArray = foods.filter((food) => food.id !== id);
+    setFoods(newFoodArray);
+    console.log(newFoodArray);
   }
 
   const foodList = foods.map((food) => (
     <li key={food.id}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+      <button onClick={() => handleLiClick(food.id)}>Remove Food</button>
     </li>
   ));
 
